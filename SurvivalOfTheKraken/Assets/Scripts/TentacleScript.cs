e9b0f29376314040538749393713f63ad7a478ac
Obiteke <thibo.delbaen@gmail.com>
2021-12-20 16:29:13 +0100

    boat + aim tentacles

--- SurvivalOfTheKraken/Assets/Scripts/TentacleScript.cs
+++ SurvivalOfTheKraken/Assets/Scripts/TentacleScript.cs
@@ -5,15 +5,34 @@ using UnityEngine.Animations;
 
 public class TentacleScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Transform _piratePos;
+    private Animator _animator;
+    [SerializeField]
+    private bool Slap = false;
+
     void Start()
     {
-        
+        _animator = gameObject.GetComponentInChildren<Animator>();
+        _piratePos = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!Slap)
+        {
+            transform.LookAt(_piratePos, Vector3.up);
+        }
+        else
+        {
+            _animator.SetBool("Slap",true);
+            StartCoroutine(SlapAnimation(_animator.GetCurrentAnimatorStateInfo(0).length));
+        }
+    }
+    IEnumerator SlapAnimation(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Slap = false;
+        _animator.SetBool("Slap", false);
     }
 }
